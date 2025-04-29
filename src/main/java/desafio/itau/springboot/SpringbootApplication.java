2586851fb469672fcdd49453bc098e5752f33595
leonardoyuan <leonardo.yuan1@gmail.com>
2025-04-29 17:02:54 -0300

    Commit de Teste

--- src/main/java/desafio/itau/springboot/SpringbootApplication.java
+++ src/main/java/desafio/itau/springboot/SpringbootApplication.java
@@ -3,6 +3,7 @@ package desafio.itau.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//eae
 @SpringBootApplication
 public class SpringbootApplication {
 
